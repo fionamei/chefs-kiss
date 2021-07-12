e14131974c2ed89d54570174677cf731e36e6f16
fionamei <fionamei131@gmail.com>
2021-07-11 23:02:31 -0400

    removed TODOs

--- backend/api/controllers/authentication.py
+++ backend/api/controllers/authentication.py
@@ -10,22 +10,10 @@ authentication = Blueprint("authentication", __name__)  # initialize blueprint
 def create_new_user():
     """ API that creates a new user
     """
-    # TODO: Find a way to receive user data as JSON and store it in a variable
-    # https://stackoverflow.com/questions/20001229/how-to-get-posted-json-in-flask
 
     content = request.get_json()
-    # print(content)
-    # print("hello")
-    # print(type(content))
-
-    # TODO: Save the data into the authentication database
-    # Documentation: https://flask-pymongo.readthedocs.io/en/latest/
-    # https://stackabuse.com/integrating-mongodb-with-flask-using-flask-pymongo
 
     mongo.db.authentication.insert_one(content)
-
-    # TODO: Return a message indicating success
-    # https://github.com/lumi-io/whyphi-flask/blob/master/api/controllers/admin_postings.py
 
     response_object = {
         "status": True,

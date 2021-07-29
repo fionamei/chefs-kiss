20229acd07ffbb7493e0f1a7f1073b9bbc39a859
Jin Young Bang <jybang@bu.edu>
2021-07-28 20:12:46 -0400

    Added routes + instructions in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,17 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path="/" component={Main} />
+          {/* Component to view all the orders --> Sharon */}
+          <Route exact path="/orders" /> 
+
+          {/* Component to view specific order with given id */}
+          <Route exact path="/orders/:order-id" />
+          
+          {/* Component to view form in order to save an order --> Daniel */}
+          <Route exact path="/save-order" />
+
+          {/* Component to view form in order to save an order --> Fiona */}
+          <Route exact path="/find-restaurants" />
         </Switch>
       </div>
     </Router>

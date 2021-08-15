a7bd65570a751c50d020172f069030b7932a2b8f
fionamei <fionamei131@gmail.com>
2021-08-15 18:47:30 -0400

    added an input bar and cards for restaurant display

--- frontend/src/components/Location.js
+++ frontend/src/components/Location.js
@@ -1,65 +1,91 @@
 import React, { useEffect, useState } from "react";
 import { Restaurants } from "./Restaurants";
 import axios from "axios";
-import { Button } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
 
 
 const Location = () => {
 
     const [location, setLocation] = useState("");
-    const [restaurant, setRestaurant] = useState([])
-    // console.log("Rendering with: ", restaurant);
+    const [restaurant, setRestaurant] = useState([]);
 
     //functions
     const locationHandler = (e) => {
         setLocation(e.target.value);
     };
+
+    // POST request to input location into API 
     const submitHandler = (e) => {
         e.preventDefault();
-
-        // POST request to input location into API 
         axios({
             method: "POST",
             url: 'http://localhost:5000/api/find-nearby-restaurants',
-            data: {"location": location},
-          })
+            data: { "location": location },
+        })
             .then((response) => {
-              //handle success
-              setRestaurant(response.data.result)
-            //   console.log(response);
+                //handle success
+                setRestaurant(response.data.result)
             })
-            .catch( err => {
-              //handle error
-              console.log("POST REQ ERROR");
+            .catch(err => {
+                //handle error
+                console.log("POST REQ ERROR");
             });
-
     };
 
+    // gets the output from api 
     useEffect(() => {
-        // gets the output from api 
         axios
             .get('http://localhost:5000/api/find-nearby-restaurants')
             .then((response) => {
-                // console.log(response)
-                // console.log(response.data.result)
                 setRestaurant(response.data.result);
-                // console.log(restaurant)
             })
             .catch(
                 (err => {
+                    //handle error
                     console.log("GET REQ ERROR!")
                 })
             );
     }, [])
 
-    // console.log(restaurant);
+    //styling for the form
+    const formStyles = makeStyles((theme) => ({
+        root: {
+            padding: '2px 4px',
+            display: 'flex',
+            alignItems: 'center',
+            width: 400,
+            margin: 'auto',
+        },
+        input: {
+            marginLeft: theme.spacing(1),
+            flex: 1,
+        },
+        iconButton: {
+            padding: 10,
+        },
+    }));
+
+    const classes = formStyles();
+
     return (
         <div>
             <h1>Search page</h1>
-            <form>
-                <input value={location} onChange={locationHandler} type="text" />
-                <Button onClick={submitHandler} color="primary" type="submit">Submit</Button>
-            </form>
+            <Paper component="form" className={classes.root}>
+                <InputBase
+                    className={classes.input}
+                    value={location}
+                    onChange={locationHandler}
+                    placeholder="Enter Location"
+                />
+                <IconButton type="submit" className={classes.iconButton} onClick={submitHandler} aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
             <Restaurants restaurant={restaurant} />
         </div>
     );

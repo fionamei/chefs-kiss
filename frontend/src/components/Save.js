d030c6c8f40a6552c2ac01e71fbd6c56a701531a
Daniel Chen <dchen7132@gmail.com>
2021-08-18 01:26:58 -0400

    added material-ui elements

--- frontend/src/components/Save.js
+++ frontend/src/components/Save.js
@@ -1,14 +1,25 @@
-import React, {useState} from "react";
+import React, { useState, setState } from "react";
 import Axios from "axios";
-import Button from '@material-ui/core/Button';
+import { Button, TextField, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    field: {
+        marginTop: 20,
+        marginBotton: 20,
+        display: "block"
+    }
+})
 
 function Save() {
     const url = "http://127.0.0.1:5000/api/create-order";
+
+    const classes = useStyles();
+
     const [data,setData] = useState({
         Name: "",
         Restaurant: "",
         Items: ""
-    })
+    });
 
     function submit(e){
         e.preventDefault();
@@ -20,22 +31,62 @@ function Save() {
         .then(res => {
             console.log(res.data)
         })
-    }
+        setData({
+            Name: "",
+            Restaurant: "",
+            Items: ""
+        })
+    };
+
     function handle(e){
         const newdata={...data}
         newdata[e.target.id] = e.target.value
         setData(newdata)
         console.log(newdata)
-    }
+    };
 
     return (
         <div>
+            <h1>Save an Order!</h1>
             <form onSubmit={(e) => submit(e)}>
-                <input onChange={(e) => handle(e)} id="Name" value={data.Name} placeholder='Name' type='text'></input>
-                <input onChange={(e) => handle(e)} id="Restaurant" value={data.Restaurant} placeholder='Restaurant' type='text'></input>
-                <input onChange={(e) => handle(e)} id="Items" value={data.Items} placeholder='Items' type='text'></input>
+                <TextField
+                    className={classes.field}
+                    onChange={(e) => handle(e)}
+                    id="Name"
+                    value={data.Name}
+                    placeholder="Name"
+                    type="text"
+                    variant="outlined"
+                    color="secondary"
+                    size="small"
+                    required
+                />
+                <TextField
+                    className={classes.field}
+                    onChange={(e) => handle(e)}
+                    id="Restaurant"
+                    value={data.Restaurant}
+                    placeholder="Restaurant"
+                    type="text"
+                    variant="outlined"
+                    color="secondary"
+                    size="small"
+                    required
+                />
+                <TextField
+                    className={classes.field}
+                    onChange={(e) => handle(e)}
+                    id="Items"
+                    value={data.Items}
+                    placeholder="Items"
+                    type="text"
+                    variant="outlined"
+                    color="secondary"
+                    size="small"
+                    required
+                />
                 <br></br>
-                <Button variant="outlined" type="submit">Submit</Button>
+                <Button variant="outlined" type="submit" size="large">Submit</Button>
             </form>
         </div>
     )
